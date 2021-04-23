fa560019286466c798d7523c929362cac95eca58
Flavio Monteiro <flaviohnm@gmail.com>
2021-04-23 01:23:11 -0300

    criando o filtro por curso

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -11,13 +11,32 @@ import { CourseService } from "./course.service";
 })
 export class CourseListComponent {
 
-  courses: Course[] = [];
+  _filteredCourses: Course[] = [];
+
+  _courses: Course[] = [];
+
+  _filterBy: string;
 
   constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
 
-      this.courses = this.courseService.retrieveAll();
+    this._courses = this.courseService.retrieveAll();
+    this._filteredCourses = this._courses;
+
+  }
+
+  set filter(value: string) {
+
+    this._filterBy = value;
+
+    this._filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+
+  }
+
+  get filter() {
+
+    return this._filterBy;
 
   }
 }
